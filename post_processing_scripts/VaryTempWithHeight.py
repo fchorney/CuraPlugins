4fcaa2cf434758e82934187de60bf1616c874f7c
Fernando Chorney <fernando.chorney@invenia.ca>
2019-01-07 17:05:18 -0600

    Fix initial temp, and add height buffer
    - Actually sets the initial temp the user selected
    - Adds a height buffer variable so that the temperature changes only
    happen above the buffer.
    - Fixes the G-Code regex to understand G0 codes with flow as well.
    - Fixed some documentation, etc.

--- post_processing_scripts/VaryTempWithHeight.py
+++ post_processing_scripts/VaryTempWithHeight.py
@@ -20,31 +20,42 @@ class VaryTempWithHeight(Script):
             'settings': {
                 'start_temperature': {
                     'label': 'Start Temperature',
-                    'description': 'Initial nozzle temperature',
+                    'description': 'Sets the initial nozzle temperature',
                     'unit': '°C',
                     'type': 'int',
-                    'default_value': 200
+                    'default_value': 225
                 },
                 'height_increment': {
                     'label': 'Height Increment',
                     'description': (
-                        'Adjust temperature each time height param '
+                        'Adjust temperature each time the z height '
                         'changes by this much'
                     ),
                     'unit': 'mm',
                     'type': 'int',
                     'default_value': 10
                 },
-                'temperature_increment': {
-                    'label': 'Temperature Increment',
+                'temperature_decrement': {
+                    'label': 'Temperature decrement',
                     'description': (
                         'Decrease temperature by this much with each '
                         'height increment'
                     ),
                     'unit': '°C',
                     'type': 'int',
-                    'default_value': 4
-                }
+                    'default_value': 5
+                },
+                'height_buffer': {
+                    'label': 'Height Buffer',
+                    'description': (
+                        'Add a buffer to the bottom of the model where no '
+                        'temperature changes will happen. Temperature changes '
+                        'will start above the height buffer that you have set'
+                    ),
+                    'unit': 'mm',
+                    'type': 'float',
+                    'default_value': 0.0
+                },
             }
         }
 
@@ -56,12 +67,15 @@ class VaryTempWithHeight(Script):
         # Grab settings variables
         start_temp = self.getSettingValueByKey('start_temperature')
         height_inc = self.getSettingValueByKey('height_increment')
-        temp_inc = self.getSettingValueByKey('temperature_increment')
+        temp_dec = self.getSettingValueByKey('temperature_decrement')
+        height_buf = self.getSettingValueByKey('height_buffer')
 
         # Set our command regex
+        # ex. G0 X60.989 Y60.989 Z1.77
+        # ex. G0 F7200 X104.295 Y112.483 Z11.1
         cmd_re = re.compile((
-            r'G[0-9]+\.?[0-9]* X[0-9]+\.?[0-9]* '
-            r'Y[0-9]+\.?[0-9]* Z([0-9]+\.?[0-9]*)'
+            r'G[0-9]+\.?[0-9]* (?:F[0-9]+\.?[0-9]* )?'
+            r'X[0-9]+\.?[0-9]* Y[0-9]+\.?[0-9]* Z([0-9]+\.?[0-9]*)'
         ))
 
         # Set initial state
@@ -70,7 +84,9 @@ class VaryTempWithHeight(Script):
         started = False
         z = 0.0
         new_temp = 0
+        set_initial = False
 
+        # Cycle through each line of G-Code
         for layer in data:
             output_line = ''
             for line in layer.split('\n'):
@@ -84,30 +100,46 @@ class VaryTempWithHeight(Script):
                     output_line += '%s\n' % line
                     continue
 
-                # Find the X,Y,Z Line (ex. G0 X60.989 Y60.989 Z1.77)
+                # Find the X,Y,Z Line
                 match = cmd_re.search(line)
 
                 # If we've found our line
                 if match is not None:
                     # Grab the z value
-                    new_z = float(match.groups()[0])
+                    new_z = float(match.groups()[0]) - height_buf
 
                     # If our z value has changed
                     if new_z != z:
                         z = new_z
+                        layer = int(z / height_inc)
+
+                        # Check if we're on the initial layer, or below our height
+                        # buffer
+                        if layer <= 0:
+                            # If we haven't written out the initial temperature yet, do
+                            # it here
+                            if not set_initial:
+                                set_initial = True
+                                output_line += write_temp(start_temp)
+
+                            # If were below the height buffer or at 0, we don't really
+                            # need to figure anything out here.
+                            break
 
                         # Determine new temperature
-                        new_temp = int(z / height_inc) * temp_inc
-                        new_temp = start_temp - new_temp
+                        new_temp = start_temp - (layer * temp_dec)
 
                         # If we hit a spot where we need to change the
                         # temperature, then write the gcode command
                         if new_temp < current_temp:
                             current_temp = new_temp
-                            output_line += ';TYPE:CUSTOM\n'
-                            output_line += 'M104 S%d\n' % new_temp
+                            output_line += write_temp(new_temp)
                 # output the current line
                 output_line += '%s\n' % line
             # Append the current possibly modified layer to the output
             output.append(output_line)
         return output
+
+
+def write_temp(temp):
+    return ';TYPE:CUSTOM\nM104 S%d\n' % temp
