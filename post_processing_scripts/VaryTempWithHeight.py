19469787dddd6a9fff6e43bbe909a4aae8568c72
Fernando Chorney <fernando.chorney@invenia.ca>
2019-01-08 09:36:21 -0600

    Make Temp script Python Black compliant

--- post_processing_scripts/VaryTempWithHeight.py
+++ post_processing_scripts/VaryTempWithHeight.py
@@ -13,50 +13,50 @@ class VaryTempWithHeight(Script):
     def getSettingDataString(self):
         # Create settings as an object
         settings = {
-            'name': 'Vary Temp With Height',
-            'key': 'VaryTempWithHeight',
-            'metadata': {},
-            'version': 2,
-            'settings': {
-                'start_temperature': {
-                    'label': 'Start Temperature',
-                    'description': 'Sets the initial nozzle temperature',
-                    'unit': '°C',
-                    'type': 'int',
-                    'default_value': 225
+            "name": "Vary Temp With Height",
+            "key": "VaryTempWithHeight",
+            "metadata": {},
+            "version": 2,
+            "settings": {
+                "start_temperature": {
+                    "label": "Start Temperature",
+                    "description": "Sets the initial nozzle temperature",
+                    "unit": "°C",
+                    "type": "int",
+                    "default_value": 225,
                 },
-                'height_increment': {
-                    'label': 'Height Increment',
-                    'description': (
-                        'Adjust temperature each time the z height '
-                        'changes by this much'
+                "height_increment": {
+                    "label": "Height Increment",
+                    "description": (
+                        "Adjust temperature each time the z height "
+                        "changes by this much"
                     ),
-                    'unit': 'mm',
-                    'type': 'int',
-                    'default_value': 10
+                    "unit": "mm",
+                    "type": "int",
+                    "default_value": 10,
                 },
-                'temperature_decrement': {
-                    'label': 'Temperature decrement',
-                    'description': (
-                        'Decrease temperature by this much with each '
-                        'height increment'
+                "temperature_decrement": {
+                    "label": "Temperature decrement",
+                    "description": (
+                        "Decrease temperature by this much with each "
+                        "height increment"
                     ),
-                    'unit': '°C',
-                    'type': 'int',
-                    'default_value': 5
+                    "unit": "°C",
+                    "type": "int",
+                    "default_value": 5,
                 },
-                'height_buffer': {
-                    'label': 'Height Buffer',
-                    'description': (
-                        'Add a buffer to the bottom of the model where no '
-                        'temperature changes will happen. Temperature changes '
-                        'will start above the height buffer that you have set'
+                "height_buffer": {
+                    "label": "Height Buffer",
+                    "description": (
+                        "Add a buffer to the bottom of the model where no "
+                        "temperature changes will happen. Temperature changes "
+                        "will start above the height buffer that you have set"
                     ),
-                    'unit': 'mm',
-                    'type': 'float',
-                    'default_value': 0.0
+                    "unit": "mm",
+                    "type": "float",
+                    "default_value": 0.0,
                 },
-            }
+            },
         }
 
         # Dump to json string
@@ -65,18 +65,20 @@ class VaryTempWithHeight(Script):
 
     def execute(self, data):
         # Grab settings variables
-        start_temp = self.getSettingValueByKey('start_temperature')
-        height_inc = self.getSettingValueByKey('height_increment')
-        temp_dec = self.getSettingValueByKey('temperature_decrement')
-        height_buf = self.getSettingValueByKey('height_buffer')
+        start_temp = self.getSettingValueByKey("start_temperature")
+        height_inc = self.getSettingValueByKey("height_increment")
+        temp_dec = self.getSettingValueByKey("temperature_decrement")
+        height_buf = self.getSettingValueByKey("height_buffer")
 
         # Set our command regex
         # ex. G0 X60.989 Y60.989 Z1.77
         # ex. G0 F7200 X104.295 Y112.483 Z11.1
-        cmd_re = re.compile((
-            r'G[0-9]+\.?[0-9]* (?:F[0-9]+\.?[0-9]* )?'
-            r'X[0-9]+\.?[0-9]* Y[0-9]+\.?[0-9]* Z([0-9]+\.?[0-9]*)'
-        ))
+        cmd_re = re.compile(
+            (
+                r"G[0-9]+\.?[0-9]* (?:F[0-9]+\.?[0-9]* )?"
+                r"X[0-9]+\.?[0-9]* Y[0-9]+\.?[0-9]* Z([0-9]+\.?[0-9]*)"
+            )
+        )
 
         # Set initial state
         output = []
@@ -88,16 +90,16 @@ class VaryTempWithHeight(Script):
 
         # Cycle through each line of G-Code
         for layer in data:
-            output_line = ''
-            for line in layer.split('\n'):
+            output_line = ""
+            for line in layer.split("\n"):
                 # If we see LAYER:0, this means we are in the main layer code
-                if 'LAYER:0' in line:
+                if "LAYER:0" in line:
                     started = True
 
                 # output any comment lines or pre-start lines
                 # without modification
-                if line.startswith(';') or not started:
-                    output_line += '%s\n' % line
+                if line.startswith(";") or not started:
+                    output_line += "%s\n" % line
                     continue
 
                 # Find the X,Y,Z Line
@@ -135,11 +137,11 @@ class VaryTempWithHeight(Script):
                             current_temp = new_temp
                             output_line += write_temp(new_temp)
                 # output the current line
-                output_line += '%s\n' % line
+                output_line += "%s\n" % line
             # Append the current possibly modified layer to the output
             output.append(output_line)
         return output
 
 
 def write_temp(temp):
-    return ';TYPE:CUSTOM\nM104 S%d\n' % temp
+    return ";TYPE:CUSTOM\nM104 S%d\n" % temp
